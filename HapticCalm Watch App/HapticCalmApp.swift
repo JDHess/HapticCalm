9595c116a0d7c258b33f007cdc235a550c02d580
Julie Hess <jul>
2022-12-29 07:54:11 -0800

    Initial Commit

---
+++ HapticCalm Watch App/HapticCalmApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticCalmApp.swift
+//  HapticCalm Watch App
+//
+//  Created by Julie Hess on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct HapticCalm_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HapticCalm/HapticCalmApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticCalmApp.swift
+//  HapticCalm
+//
+//  Created by Julie Hess on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct HapticCalmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
